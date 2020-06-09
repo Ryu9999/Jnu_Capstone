cae7e73ff45da63cb31844fef57b7e6e15b0c43e
Ryu9999 <62986263+Ryu9999@users.noreply.github.com>
2020-06-09 22:39:14 +0900

    Update lexicon_sentiment_analysis.py

--- lexicon_sentiment_analysis.py
+++ lexicon_sentiment_analysis.py
@@ -48,55 +48,35 @@ avg_V = 5.06    # average V from ANEW dict
 avg_A = 4.21
 avg_D = 5.18
 
-# performs sentiment analysis on inputFile using the ANEW database, outputting results to a new CSV file in outputDir
+# use anew lexicon, get sentiment values(Valencd, Arousal) and get .csv output file
 def analyzefile(input_file, output_dir, mode):
-    """
-    Performs sentiment analysis on the text file given as input using the ANEW database.
-    Outputs results to a new CSV file in output_dir.
-    :param input_file: path of .txt file to analyze
-    :param output_dir: path of directory to create new output file
-    :param mode: determines how sentiment values for a sentence are computed (median or mean)
-    :return:
-    """
-    output_file = os.path.join(output_dir, os.path.basename(input_file).rstrip('.txt') + ".csv") #"OutputAnewSentiment_" + 
-    # make buffer for list of utterance
+
+    output_file = os.path.join(output_dir, os.path.basename(input_file).rstrip('.txt') + ".csv") + 
     utterances = []
-    # read file into string
     with open(input_file, 'r',encoding='latin-1') as myfile:
-        #for line in myfile.readlines():
-            #utterance = tokenize.word_tokenize(line)
-            #utterances = np.append(utterances, utterance)
-            #utterances.append(utterance)
-        
-        # writing file
-        i = 1 # to store sentence/line index
-        # check each word in sentence/line for sentiment and write to output_file
+        i = 1 
         with open(output_file, 'w', -1, 'utf-8') as csvfile:
             fieldnames = ['Sentence ID', 'Sentence', 'Valence', 'Arousal', 'Dominance', 'Sentiment Label',
                           'Average VAD', '# Words Found', 'Found Words', 'All Words']
             writer = csv.DictWriter(csvfile, delimiter=';', fieldnames=fieldnames)
             writer.writeheader()
-            # analyze each sentence/line for sentiment
+            # analyze sentence per line
             for line in myfile.readlines():
                 s = tokenize.word_tokenize(line.lower())
-               #print("S" + str(i) ,": "  ,s), S: sentence, s: tokened words
                 all_words = []
                 found_words = []
                 total_words = 0
-                v_list = []  # holds valence scores
-                a_list = []  # holds arousal scores
-                d_list = []  # holds dominance scores
+                v_list = []  # hold Valence, Arousal, Dominace value
+                a_list = []  
+                d_list = []  
 
-                # search for each valid word's sentiment in ANEW
-                words = nltk.pos_tag(s)
-                for index, p in enumerate(words): #순서가 있는 자료형을 입력으로 받아 인덱스 값을 포함하는 enumerate 객체를 리턴
-                    # don't process stops or words w/ punctuation
-                    w = p[0] #p에 words
+                # search words in ANEW
+                words = nltk.pos_tag(s) #tokenized words to tagging(by identifying part of speech)
+                for index, p in enumerate(words): #순서가 있는 자료형을 입력으로 받아 인덱스 값을 포함하는 enumerate 객체 리턴
+                    w = p[0] #p의 words
                     pos = p[1]
                     if w in stops or not w.isalpha():
                         continue
-
-                    # check for negation in 3 words before current word
                     j = index-1
                     neg = False
                     while j >= 0 and j >= index-3:
@@ -105,19 +85,17 @@ def analyzefile(input_file, output_dir, mode):
                             break
                         j -= 1
     
-                    # lemmatize word based on pos
+                    # lemmatize word (base on part of speech)
                     if pos[0] == 'N' or pos[0] == 'V':
                         lemma = lmtzr.lemmatize(w, pos=pos[0].lower())#lemmatize 표제어 추출(are, is, am의 뿌리는 be, 이를 표제어라고 한다.)
                     else:
                         lemma = w
     
-                    all_words.append(lemma)
-    
-                    # search for lemmatized word in ANEW
+                    all_words.append(lemma)                      
                     with open(anew) as csvfile:
                         reader = csv.DictReader(csvfile)
                         for row in reader:
-                            if row['Word'].casefold() == lemma.casefold():#casefold 대소문자 구분 제거 메소드
+                            if row['Word'].casefold() == lemma.casefold():#casefold 대소문자 구분 제거
                                 if neg:
                                     found_words.append("neg-"+lemma)
                                 else:
@@ -159,14 +137,14 @@ def analyzefile(input_file, output_dir, mode):
                         arousal = statistics.mean(a_list)
                         dominance = statistics.mean(d_list)
                     elif mode == 'mika': # arousal, valence, dominance calculate
-                        # calculate valence
+                        #valence cal
                         if statistics.mean(v_list) < avg_V: #v_list 의 평균값이 avg_V 보다 작으면 최대값-평균값
                             sentiment = max(v_list) - avg_V
                         elif max(v_list) < avg_V:
                             sentiment = avg_V - min(v_list)
                         else:
                             sentiment = max(v_list) - min(v_list)
-                        # calculate arousal
+                        #arousal cal
                         if statistics.mean(a_list) < avg_A:
                             arousal = max(a_list) - avg_A
                             print(arousal)
@@ -198,17 +176,6 @@ def analyzefile(input_file, output_dir, mode):
 
 
 def main(input_file, input_dir, output_dir, mode):
-    """
-    Runs analyzefile on the appropriate files, provided that the input paths are valid.
-    :param input_file:
-    :param input_dir:
-    :param output_dir:
-    :param mode:
-    :return:
-    """
-    print("input_file length=",len(input_file))
-    print("input_dir length=", len(input_dir))
-    print(mode)
     if len(output_dir) < 0 or not os.path.exists(output_dir):  # empty output
         print('No output directory specified, or path does not exist')
         sys.exit(0)
@@ -225,7 +192,7 @@ def main(input_file, input_dir, output_dir, mode):
         if os.path.isdir(input_dir):
             directory = os.fsencode(input_dir)
             for file in os.listdir(directory): #주어진 디렉터리에 있는 항목들의 이름을 담고 있는 리스트 반환
-                filename = os.path.join(input_dir, os.frowssdecode(file)) #fsdecode 파일 경로 리턴하는 메소드
+                filename = os.path.join(input_dir, os.frowssdecode(file)) #fsdecode 파일 경로 리턴
                 print("filename is : ",filename)
                 if filename.endswith(".txt"):
                     start_time = time.time()
@@ -238,16 +205,15 @@ def main(input_file, input_dir, output_dir, mode):
 
 
 if __name__ == '__main__':
-    # get arguments below:
+    # input Data with main fun parameter
     input_file = (r'C:\Users\user\originalData\originalData4.txt')
-    print("input file is =",input_file)
     input_dir = (r'C:\Users\user\originalData')
     mode = 'mika'
     output_dir = r'C:\Users\user\SentimentAnalysis-master\SentimentAnalysis-master\anew_' + mode
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
                                                                                     
-        # run main with arguments above
+        # run main 
 main(input_file, input_dir, output_dir, mode)
 
 
